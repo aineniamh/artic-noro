67761eabf6c073928955f92379c0bfc7c498bc11
aineniamh <aine.otoole@ed.ac.uk>
2019-07-29 09:00:37 +0100

    deleting binlorry from repo

--- binlorry/binlorry-runner.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Convenience wrapper for running Binlorry directly from source tree.
-"""
-
-from binlorry.binlorry import main
- 
-if __name__ == '__main__':
-    main()
--- binlorry/build/lib/binlorry/version.py
+++
@@ -1,5 +0,0 @@
-"""
-The version is stored here in a separate file so it can exist in only one place.
-http://stackoverflow.com/questions/458550
-"""
-__version__ = '0.1.0'
